d6060b6acf00f40b024d95560fb3f50b46cabb95
Adriano Izquierdo <128609907+AdrianoIzquierdo@users.noreply.github.com>
2024-10-15 18:50:05 -0300

    Update models.py
    agragado de clase compra

--- ms-catalogo/app/models.py
+++ ms-catalogo/app/models.py
@@ -13,3 +13,17 @@ class Producto(db.Model):
 
     def __repr__(self):
         return f"<Producto {self.nombre}>"
+
+class Compra(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    producto_id = db.Column(db.Integer, nullable=False)
+    usuario_id = db.Column(db.Integer, nullable=False)
+    cantidad = db.Column(db.Integer, default=1)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "producto_id": self.producto_id,
+            "usuario_id": self.usuario_id,
+            "cantidad": self.cantidad
+        }
