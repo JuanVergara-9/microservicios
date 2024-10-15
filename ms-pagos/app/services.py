b4dc652587ab1bdcf137320fba3260af455771ea
Adriano Izquierdo <128609907+AdrianoIzquierdo@users.noreply.github.com>
2024-10-15 19:35:38 -0300

    Update services.py

--- ms-pagos/app/services.py
+++ ms-pagos/app/services.py
@@ -3,6 +3,22 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.models import Pago, db
 from app.saga import SagaEvent, SagaState
 
+
+def registrar_pago(usuario_id, monto, metodo_pago):
+    nuevo_pago = Pago(usuario_id=usuario_id, monto=monto, metodo_pago=metodo_pago)
+    db.session.add(nuevo_pago)
+    db.session.commit()  # Guardamos el pago en la base de datos
+    return {"status": "success", "pago": nuevo_pago}
+
+def actualizar_estado_pago(pago_id, estado):
+    pago = Pago.query.get(pago_id)
+    if pago:
+        pago.estado = estado
+        db.session.commit()  # Actualizamos el estado del pago en la base de datos
+        return {"status": "success", "pago": pago}
+    else:
+        return {"status": "error", "message": "Pago no encontrado"}
+
 def manejar_evento_procesar_pago(compra_id, usuario_id, monto, metodo_pago):
     try:
         nuevo_pago = Pago(usuario_id=usuario_id, monto=monto, metodo_pago=metodo_pago)
