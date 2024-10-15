2218ae69007f02188d736654121ae8300b3be405
Adriano Izquierdo <128609907+AdrianoIzquierdo@users.noreply.github.com>
2024-10-15 19:34:53 -0300

    Update services.py

--- ms-inventario/app/services.py
+++ ms-inventario/app/services.py
@@ -3,6 +3,24 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.models import Producto, db
 from app.saga import SagaEvent, SagaState
 
+def actualizar_stock(producto_id, cantidad):
+    producto = Producto.query.get(producto_id)
+    if producto and producto.stock >= cantidad:
+        producto.stock -= cantidad  # Reducimos el stock
+        db.session.commit()
+        return {"status": "success", "producto": producto}
+    else:
+        return {"status": "error", "message": "Stock insuficiente o producto no encontrado"}
+
+def aumentar_stock(producto_id, cantidad):
+    producto = Producto.query.get(producto_id)
+    if producto:
+        producto.stock += cantidad  # Aumentamos el stock
+        db.session.commit()
+        return {"status": "success", "producto": producto}
+    else:
+        return {"status": "error", "message": "Producto no encontrado"}
+
 def manejar_evento_reservar_inventario(compra_id, producto_id, cantidad):
     try:
         producto = Producto.query.get(producto_id)
